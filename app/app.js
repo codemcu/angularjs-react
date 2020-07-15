e355d6fa9a9c811ad509a9d39dd04c168ba9158e
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-15 20:40:14 +0200

    angular seed

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
 'use strict';
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var myApp = angular.module('myApp', [])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+myApp.controller('ReactController',
+    function($scope) {
+      $scope.title = 'react'
+    })
+myApp.directive('reactComponent',
+    function () {
+      return {
+        restrict: 'E',
+        template: "<input type='text' name='dates' />"
+      }
+    })
